02aadb8dc73a742080fc121a242bf08fafec193f
mansijoshi17 <mansijoshi1777@gmail.com>
2023-03-31 18:47:16 +0530

    Contract deployed

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export const trustifiedContracts = {
   fvmtestnet: {
-    trustified: "0xE1b8A9e8F5492C5242DE5c6A59C5E7ff1145891a",
+    trustified: "0x0532fE3b5acB8451A61D998C64895745d9642804",
   }, // Testnet
   fvm: {
     trustified: "0x0867c907Ae1d7eB43c381BbdA494E61B77202BFa",
   }, // Mainnet
-  mumbai: {
-    trustified: "0xA7683AEDEcECc2C85EBB6D0f93a1AE852bBeA077",
-  },
-  bsc: {
-    trustified: "0x0FE82cBB448A89Dd912EbC4117B927616826AcCC",
-  },
+  // mumbai: {
+  //   trustified: "0xA7683AEDEcECc2C85EBB6D0f93a1AE852bBeA077",
+  // },
+  // bsc: {
+  //   trustified: "0x0FE82cBB448A89Dd912EbC4117B927616826AcCC",
+  // },
 };
 
 export const chain = {
